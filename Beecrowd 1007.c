033a60e353f7f5e07a3993fb34cd0321d246e7e4
Marina Villaça <62123518+marinavillaca@users.noreply.github.com>
2022-03-18 21:01:54 -0300

    Create Beecrowd 1007.c

---
+++ Beecrowd 1007.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C, D, diferenca;
+
+    scanf("%d %d %d %d", &A, &B, &C, &D);
+
+    diferenca = (A*B - C*D);
+
+    printf("DIFERENCA = %d\n", diferenca);
+
+    return 0;
+}
